02018769119fb1ad49611e9d51302bb0aca35c28
d-smith <doug.smith.mail@gmail.com>
2018-01-19 11:28:16 -0800

    Implemented subscription lookup

--- smspub.go
+++ smspub.go
@@ -6,22 +6,87 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"os"
+	"log"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
-func handleRequest(ctx context.Context, e events.DynamoDBEvent) {
+var (
+	subTable = os.Getenv("SUB_TABLE")
+)
 
-	for _, record := range e.Records {
-		fmt.Printf("Processing request data for event ID %s, type %s.\n", record.EventID, record.EventName)
+type AWSContext struct {
+	ddbSvc dynamodbiface.DynamoDBAPI
+}
 
-		// Print new values for attributes of type String
-		for name, value := range record.Change.NewImage {
-			if value.DataType() == events.DataTypeString {
-				fmt.Printf("Attribute name: %s, value: %s\n", name, value.String())
+
+
+
+func notify(awsContext *AWSContext,instanceId, state string) error {
+
+	log.Printf("Looking for subscriptions for instance id %s\n", instanceId)
+
+	input := &dynamodb.QueryInput{
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":v1": {
+				S: aws.String(instanceId),
+			},
+		},
+		KeyConditionExpression: aws.String("InstanceID = :v1"),
+		TableName:              aws.String(subTable),
+	}
+
+	result, err := awsContext.ddbSvc.Query(input)
+	if err != nil {
+		return err
+	}
+
+	items := result.Items
+	if len(items) == 0 {
+		log.Println("No subscribers to notify")
+		return nil
+	}
+
+	for _, item := range items {
+		notifyDestination := item["Notify"].S
+		log.Printf("Notify %s\n", *notifyDestination)
+	}
+
+	return nil
+}
+
+func makeHandler(awsContext *AWSContext) func(ctx context.Context, e events.DynamoDBEvent) {
+	return func(ctx context.Context, e events.DynamoDBEvent) {
+
+		for _, record := range e.Records {
+			fmt.Printf("Processing request data for event ID %s, type %s.\n", record.EventID, record.EventName)
+
+
+
+			instanceId := record.Change.NewImage["instanceId"].String()
+			state := record.Change.NewImage["instanceId"].String()
+
+			err := notify(awsContext, instanceId, state)
+			if err != nil {
+				log.Printf("Error notifying subscribers: %s\n", err.Error())
 			}
 		}
 	}
 }
 
+
+
 func main() {
-	lambda.Start(handleRequest)
+	var awsContext AWSContext
+
+	sess := session.New()
+	svc := dynamodb.New(sess)
+
+	awsContext.ddbSvc = svc
+
+	handler := makeHandler(&awsContext)
+	lambda.Start(handler)
 }
